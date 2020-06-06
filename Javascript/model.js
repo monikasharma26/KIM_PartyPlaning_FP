9c9e56135b273a30ecebe153f924d51c36bd0a64
monikasharma26 <monikasharma26081991@gmail.com>
2020-06-06 13:34:32 -0400

    [monika] model class added

---
+++ Javascript/model.js
@@ -0,0 +1,30 @@
+//Model Class to create Constructor
+class  User {
+    constructor(fname,lname,password,email,username) {
+      this.fname = fname;
+      this.lname = lname;
+      this.password = password;
+      this.email = email;
+      this.username = username;
+    }
+  }
+  class SendMessage {
+    constructor(fname,lname,email,mobile,msgid){
+      this.fname = fname;
+      this.lname = lname;
+       this.email = email;
+       this.mobile = mobile;
+       this.msgid = msgid;
+      
+    }
+  }
+  class Booking {
+    constructor(fname,lname,emailid,mobile,msgid,userId) {
+      this.fname = fname;
+      this.lname = lname;
+      this.emailid = emailid;
+       this.mobile = mobile;
+       this.msgid = msgid;
+       this.userId= userId;
+    }
+  }
